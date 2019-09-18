70805ea4f3ccb0d22535d8161a7df1043ddeaed7
yamazaki_yohei <y_yamazaki@timedia.co.jp>
2019-09-18 19:04:18 +0900

    add vscode settings

--- setup.sh
+++ setup.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 DOT_FILES=(.bashrc .bash_profile)
+VS_CODE_FILES=(settings.json keybindings.json)
 
 for file in ${DOT_FILES[@]}
 do
   ln -s $HOME/dotfiles/$file $HOME/$file
+done
+
+for file in ${VS_CODE_FILES[@]}
+do
+  ln -s $HOME/dotfiles/vscode/$file $HOME/Library/Application\ Support/Code/User/$file
 done
